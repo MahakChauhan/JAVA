aa1597526746f0e0e954110b68cab5f1f9501b62
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-03 17:53:51 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,12 @@
+package pk1; 
+//packages is used to group related classes .
+class Test
+/*class is a collection of similar functions
+we use PascalConvention to write class name and camelCaseConvention to write function name */
+ {
+    public static void main(String args[]) 
+    //we can group similar code in functions
+    {
+        System.out.println("my first java program")
+    }
+}
