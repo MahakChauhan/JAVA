63f41447a848f702ffc4631dd04c64288753f4fc
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-04 21:19:19 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,29 @@
+class Test{
+    public static void main(String[] args) {
+       /* arithmetic operator
+        int a=16;
+        int b=2;
+        int c=a+b;
+        System.out.println(c);
+        int c1=a-b;
+        System.out.println(c1);
+        int c2=a/b;
+        System.out.println(c2);
+        int c3=a%b;
+        System.out.println(c3);
+        int c4=3;
+         c4+=5;
+        System.out.println(c4);
+        */
+        //assignment operator
+        int a=6;
+        System.out.println(a);
+       //logical operator
+       System.out.println(80>20&&90>30);
+       System.out.println(20>30|20<30);
+       //comparison operator
+       System.out.println(36<10);
+       System.out.println(36>=36);
+
+    }
+}
