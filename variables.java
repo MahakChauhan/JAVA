324948cfee7339a203052dcb39f2a0c57c1fb611
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-04 17:44:06 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,25 @@
+/* packages is used to group related classes .
+class Test
+class is a collection of similar functions
+we use PascalConvention to write class name and camelCaseConvention to write function name 
+ {
+   public static void main(String args[]) 
+    we can group similar code in functions
+    {
+        System.out.println("my first java program");
+    }
+}
+*/
+class Test{
+    public static void main(String[] args) {
+        //valid declartion of variables
+        int money=20;
+        int money12=10;
+        int money_10=50;
+        int money$=10;
+        System.out.println(money);
+        System.out.println(money12);
+        System.out.println(money_10);
+        System.out.println(money$);
+    }
+}
