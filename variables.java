fa6bd117e688d86a48f3393b377a28c1932c587c
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-04 17:47:32 +0530

    Update variables.java

--- variables.java
+++ variables.java
@@ -1,15 +1,4 @@
-/* packages is used to group related classes .
-class Test
-class is a collection of similar functions
-we use PascalConvention to write class name and camelCaseConvention to write function name 
- {
-   public static void main(String args[]) 
-    we can group similar code in functions
-    {
-        System.out.println("my first java program");
-    }
-}
-*/
+
 class Test{
     public static void main(String[] args) {
         //valid declartion of variables
