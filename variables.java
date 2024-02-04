4f227c4d72f63e5081ff14739dde4978acbfe95b
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-04 17:50:46 +0530

    Update variables.java
    Variable  is  like a container which is used to store any value.

--- variables.java
+++ variables.java
@@ -6,6 +6,7 @@ class Test{
         int money12=10;
         int money_10=50;
         int money$=10;
+        // int 12money;  it is invalid declaration of variable
         System.out.println(money);
         System.out.println(money12);
         System.out.println(money_10);
