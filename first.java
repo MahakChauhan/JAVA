ef9831cde1de529e8dfc8d8bca29611393fd4944
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-04 17:52:43 +0530

    Update first.java

--- first.java
+++ first.java
@@ -7,6 +7,6 @@ we use PascalConvention to write class name and camelCaseConvention to write fun
     public static void main(String args[]) 
     //we can group similar code in functions
     {
-        System.out.println("my first java program")
+        System.out.println("my first java program");
     }
 }
