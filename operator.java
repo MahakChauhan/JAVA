c909687eb93bd5f1d774be471a139898b8941f08
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-05 14:11:42 +0530

    Update operator.java

--- operator.java
+++ operator.java
@@ -1,6 +1,7 @@
 class Test{
     public static void main(String[] args) {
-       /* arithmetic operator
+       //Operators are used to  perform operations on varaibales and values.
+        /* arithmetic operator can not work with boolean values.
         int a=16;
         int b=2;
         int c=a+b;
@@ -9,7 +10,7 @@ class Test{
         System.out.println(c1);
         int c2=a/b;
         System.out.println(c2);
-        int c3=a%b;
+        int c3=a%b; //modulo operator
         System.out.println(c3);
         int c4=3;
          c4+=5;
@@ -18,12 +19,17 @@ class Test{
         //assignment operator
         int a=6;
         System.out.println(a);
+        int a1=6;
+        int a1+=12'
+        System.out.println(a1);
        //logical operator
        System.out.println(80>20&&90>30);
        System.out.println(20>30|20<30);
-       //comparison operator
+       //comparison operator returns boolean value.
        System.out.println(36<10);
+        System.out.println(36>10);
        System.out.println(36>=36);
+         System.out.println(36<=36);
 
     }
 }
