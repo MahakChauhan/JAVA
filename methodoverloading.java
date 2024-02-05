6a9b30995930a82fe6e52d97d43a838335c033ba
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-05 17:54:00 +0530

    Add files via upload

---
+++ methodoverloading.java
@@ -0,0 +1,28 @@
+
+class Test{
+  /* static void change(int a){
+      a=12;
+   }
+   static void change2(int[] arr){
+    arr[0]=10;
+   }
+    public static void main(String[] args) {
+        int x=30;
+        change(x);     //it will not change value of x
+        System.out.println(x);
+        int[] a={1,2,3,4}; //incase of arrays the referenced is passed. It is same case as object passing to methods.
+        change2(a);
+        System.out.println(a[0]);
+        */
+        // Two or more methods can have same name but different parameters ,its called method overloading .
+        static void fun(){
+            System.out.println("hey");
+        }
+        static void fun(int x,int y){
+            System.out.println("sum "+ (x+y) );
+        }
+        public static void main(String[] args) {
+           fun();
+           fun(2,3);
+        }
+    }
