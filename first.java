0b639f5fb6fb7abab1dabdd04aae9ac05b51295f
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-05 13:34:22 +0530

    Update first.java

--- first.java
+++ first.java
@@ -1,12 +1,13 @@
 package pk1; 
-//packages is used to group related classes .
-class Test
-/*class is a collection of similar functions
-we use PascalConvention to write class name and camelCaseConvention to write function name */
+//packages is used to group related classes.
+class Test //class is a collection of similar functions
  {
-    public static void main(String args[]) 
-    //we can group similar code in functions
+  //we can group similar code in functions.Class is a template which is used to create object.
+  //main is a default function in every java program and  main method is the entry point into the application.
+  public static void main(String args[]) 
     {
-        System.out.println("my first java program");
+     //Write your code here
+         System.out.println("my first java program");
     }
 }
+/*we use PascalConvention to write class name and camelCaseConvention to write function name */
